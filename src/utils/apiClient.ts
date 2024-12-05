1a6af5f08d0844eaf001ee67547b006bba12f010
John Damilola Ogungbure <jogungbure@gmail.com>
2024-12-05 11:56:26 +0100

    env.example

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL,
+  baseURL: import.meta.env.REACT_APP_API_BASE_URL + "/api/v1",
   headers: { "Content-Type": "application/json" },
 })
 
